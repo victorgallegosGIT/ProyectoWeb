9065284dcdbe06fe29cf06bc8ec5eb492e33963b
victorgallegosGIT <victorgallegos@iresm.edu.ar>
2021-06-23 00:39:25 -0300

    boton enviar

--- ProyectoWeb/ProyectoWeb/settings.py
+++ ProyectoWeb/ProyectoWeb/settings.py
@@ -127,6 +127,12 @@ STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT= BASE_DIR / 'media' #os.path.jcin
 
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER=""
+EMAIL_HOST_PASSWORD=""
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
